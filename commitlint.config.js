ef9ea6796f2e41efb89d714836f233243fea5106
anthony <924358746@qq.com>
2021-12-08 22:22:40 +0800

    feat(commmitlint.config.js): 测试一下

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Date: 2021-12-08 20:56:18
+ * @Author: handsome_anthony
+ * @LastEditors: handsome_anthony
+ * @Description:
+ */
+
+module.exports = {
+    // 符合angular的校验规则
+  extends: ["@commitlint/config-conventional"],
+};
